fd60828575c1455c1a69d3d11059d60b1f5349ae
huangs0 <59648015+huangs0@users.noreply.github.com>
2023-01-02 14:17:35 +0800

    Initial Commit

---
+++ HOF2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HOF2
+//
+//  Created by Huang Songlin on 2/1/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HOF2/HOF2App.swift
@@ -0,0 +1,17 @@
+//
+//  HOF2App.swift
+//  HOF2
+//
+//  Created by Huang Songlin on 2/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct HOF2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
